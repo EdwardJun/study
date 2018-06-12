6bfb4170260d3535d3c67232f1f4f2c19e88aa82
wendj <wendj@xiaomodo.com>
2018-06-12 12:29:04 +0800

    update

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ export default {
     config: {
         // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
         pages: [
-            'pages/index/main',
+            '^pages/index/main',
             'pages/login/main',
             'pages/logs/main',
             'pages/tech/main',
@@ -40,7 +40,7 @@ export default {
             'pages/project/main',
             'pages/test/main',
             'pages/test1/main',
-            '^pages/test2/main'
+            'pages/test2/main'
         ],
         window: {
             backgroundTextStyle: 'light',
